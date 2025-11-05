28d01521d0fee73a669e6cd4a045198524316e05
Richard Munthali <richardmunthali016@gmail.com>
2025-11-05 14:18:37 +0200

    Add a Services component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
+import { Services } from './components/Services';
 // import Projects from "./components/Projects/Projects";
 // import Contact from "./components/Contact/Contact";
 
@@ -21,6 +22,8 @@ function App() {
           <Projects />
           <Contact />
           */}
+
+          <Services />
         </main>
       </div>
     </ThemeProvider>
