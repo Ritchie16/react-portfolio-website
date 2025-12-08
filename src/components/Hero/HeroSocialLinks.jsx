2b36f16b6c815f6e1a26c8c58d3a81b16728cd03
Richard Munthali <richardmunthali016@gmail.com>
2025-12-08 22:17:36 +0200

    remove social links here and add to constant file

--- src/components/Hero/HeroSocialLinks.jsx
+++ src/components/Hero/HeroSocialLinks.jsx
@@ -2,41 +2,10 @@ import React from "react";
 import { motion as Motion } from "framer-motion";
 import { FaGithub, FaLinkedin, FaWhatsappSquare, FaFacebook } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import links from "../../constants/socialLinks";
 
 const HeroSocialLinks = () => {
-  // Social media links data with brand colors
-  const links = [
-    { 
-      icon: FaGithub, 
-      href: "https://github.com/Ritchie16", 
-      label: "GitHub", 
-      color: "#333333" // GitHub black
-    },
-    {
-      icon: FaLinkedin,
-      href: "https://www.linkedin.com/in/richard-munthali-692880305/",
-      label: "LinkedIn",
-      color: "#0077B5" // LinkedIn blue
-    },
-    { 
-      icon: FaWhatsappSquare, 
-      href: "https://wa.me/265880120328", 
-      label: "WhatsApp",
-      color: "#25D366" // WhatsApp green
-    },
-    { 
-      icon: FaFacebook, 
-      href: "https://www.facebook.com/richard.munthali.779", 
-      label: "Facebook",
-      color: "#1877F2" // Facebook blue
-    },
-    {
-      icon: MdEmail, 
-      href: "mailto:richardmunthali016@gmail.com", 
-      label: "Email",
-      color: "#EA4335" // Gmail red
-    },
-  ];
+ 
 
   return (
     <div className="flex items-center justify-center gap-x-6 h-20">
